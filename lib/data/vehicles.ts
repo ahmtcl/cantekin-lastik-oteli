f137facb3ee0b5cb41eaa7bf07c18f9927ab45c7
ahmtcl <ahmtcl3@gmail.com>
2026-07-09 12:37:30 +0300

    Update vehicle brands and models: Add 18 new brands, 200+ models for 2026

--- lib/data/vehicles.ts
+++ lib/data/vehicles.ts
@@ -2,54 +2,182 @@ export const vehicleBrands = [
   "Alfa Romeo",
   "Audi",
   "BMW",
+  "BYD",
   "Chery",
   "Chevrolet",
   "Citroën",
+  "Cupra",
   "Dacia",
+  "DS Automobiles",
   "Fiat",
   "Ford",
+  "Geely",
+  "GWM",
   "Honda",
   "Hyundai",
+  "Isuzu",
+  "Jaguar",
+  "Jeep",
   "Kia",
+  "Land Rover",
+  "Lexus",
   "Mazda",
   "Mercedes-Benz",
+  "MG",
+  "Mini",
   "Mitsubishi",
   "Nissan",
   "Opel",
+  "Ora",
   "Peugeot",
+  "Porsche",
   "Renault",
   "Seat",
   "Skoda",
+  "Ssangyong",
+  "Subaru",
   "Suzuki",
+  "Tesla",
+  "Togg",
   "Toyota",
   "Volkswagen",
   "Volvo",
 ];
 
 export const vehicleModels: Record<string, string[]> = {
-  "Alfa Romeo": ["Giulia", "Giulietta", "Stelvio", "Tonale"],
-  "Audi": ["A1", "A3", "A4", "A5", "A6", "A7", "A8", "Q2", "Q3", "Q5", "Q7", "Q8", "e-tron"],
-  "BMW": ["1 Serisi", "2 Serisi", "3 Serisi", "4 Serisi", "5 Serisi", "7 Serisi", "X1", "X3", "X5", "X6", "X7", "iX"],
-  "Chery": ["Tiggo", "Tiggo 7", "Tiggo 8"],
-  "Chevrolet": ["Aveo", "Cruze", "Captiva", "Trax"],
-  "Citroën": ["C3", "C3 Aircross", "C4", "C5 Aircross", "Berlingo"],
-  "Dacia": ["Duster", "Sandero", "Logan", "Lodgy", "Dokker"],
-  "Fiat": ["500", "500X", "Egea", "Tipo", "Panda", "Doblo"],
-  "Ford": ["Fiesta", "Focus", "Mondeo", "Kuga", "Puma", "Ranger", "Transit", "Tourneo"],
-  "Honda": ["Civic", "CR-V", "HR-V", "Jazz", "Accord"],
-  "Hyundai": ["i10", "i20", "i30", "Accent", "Elantra", "Tucson", "Kona", "Santa Fe", "Bayon"],
-  "Kia": ["Picanto", "Rio", "Ceed", "Sportage", "Sorento", "Stonic", "XCeed"],
-  "Mazda": ["2", "3", "6", "CX-3", "CX-5", "CX-30", "CX-60"],
-  "Mercedes-Benz": ["A-Class", "C-Class", "E-Class", "S-Class", "GLA", "GLB", "GLC", "GLE", "GLS", "EQA", "EQC"],
-  "Mitsubishi": ["ASX", "Eclipse Cross", "Outlander", "L200"],
-  "Nissan": ["Micra", "Juke", "Qashqai", "X-Trail", "Navara"],
-  "Opel": ["Corsa", "Astra", "Insignia", "Crossland", "Grandland", "Mokka", "Combo"],
-  "Peugeot": ["208", "2008", "308", "3008", "408", "5008", "Rifter", "Partner"],
-  "Renault": ["Clio", "Megane", "Taliant", "Captur", "Kadjar", "Austral", "Koleos", "Kangoo"],
+  "Alfa Romeo": ["Giulia", "Giulietta", "Stelvio", "Tonale", "Junior"],
+  
+  "Audi": [
+    "A1", "A3", "A4", "A5", "A6", "A7", "A8",
+    "Q2", "Q3", "Q4 e-tron", "Q5", "Q7", "Q8", "Q8 e-tron",
+    "e-tron GT", "RS e-tron GT"
+  ],
+  
+  "BMW": [
+    "1 Serisi", "2 Serisi", "3 Serisi", "4 Serisi", "5 Serisi", "6 Serisi", "7 Serisi", "8 Serisi",
+    "X1", "X2", "X3", "X4", "X5", "X6", "X7",
+    "iX1", "iX3", "i4", "i5", "i7", "iX"
+  ],
+  
+  "BYD": ["Atto 3", "Dolphin", "Seal", "Han", "Tang"],
+  
+  "Chery": ["Tiggo 4", "Tiggo 7", "Tiggo 8", "Tiggo 8 Pro", "Omoda 5"],
+  
+  "Chevrolet": ["Aveo", "Cruze", "Captiva", "Trax", "Silverado"],
+  
+  "Citroën": ["C3", "C3 Aircross", "C4", "C4 X", "C5 Aircross", "Berlingo", "ë-C4"],
+  
+  "Cupra": ["Formentor", "Leon", "Ateca", "Born", "Tavascan"],
+  
+  "Dacia": ["Sandero", "Sandero Stepway", "Duster", "Jogger", "Spring"],
+  
+  "DS Automobiles": ["DS 3", "DS 4", "DS 7"],
+  
+  "Fiat": ["500", "500e", "500X", "600e", "Egea", "Tipo", "Panda", "Doblo"],
+  
+  "Ford": [
+    "Fiesta", "Focus", "Mondeo", "Mustang", "Mustang Mach-E",
+    "Puma", "Kuga", "Explorer", "Bronco",
+    "Ranger", "Transit", "Tourneo", "Tourneo Courier"
+  ],
+  
+  "Geely": ["Coolray", "Azkarra", "Geometry C"],
+  
+  "GWM": ["Ora 03", "Wingle", "Poer", "Tank 300", "Tank 500"],
+  
+  "Honda": ["Civic", "CR-V", "HR-V", "Jazz", "Accord", "e:Ny1"],
+  
+  "Hyundai": [
+    "i10", "i20", "i30", "Accent", "Elantra", "Bayon",
+    "Kona", "Tucson", "Santa Fe", "Palisade",
+    "Ioniq 5", "Ioniq 6", "Kona Electric"
+  ],
+  
+  "Isuzu": ["D-Max", "MU-X"],
+  
+  "Jaguar": ["E-Pace", "F-Pace", "I-Pace", "F-Type"],
+  
+  "Jeep": ["Renegade", "Compass", "Cherokee", "Grand Cherokee", "Wrangler", "Avenger"],
+  
+  "Kia": [
+    "Picanto", "Rio", "Stonic", "XCeed", "Ceed", "Proceed",
+    "Sportage", "Sorento", "EV6", "EV9", "Niro"
+  ],
+  
+  "Land Rover": [
+    "Defender", "Discovery", "Discovery Sport",
+    "Range Rover", "Range Rover Sport", "Range Rover Evoque", "Range Rover Velar"
+  ],
+  
+  "Lexus": ["UX", "NX", "RX", "ES", "IS", "LC", "LX"],
+  
+  "Mazda": ["2", "3", "6", "CX-3", "CX-30", "CX-5", "CX-60", "CX-80", "MX-30"],
+  
+  "Mercedes-Benz": [
+    "A-Class", "B-Class", "C-Class", "E-Class", "S-Class", "CLA", "CLS",
+    "GLA", "GLB", "GLC", "GLE", "GLS", "G-Class",
+    "EQA", "EQB", "EQC", "EQE", "EQS", "EQS SUV"
+  ],
+  
+  "MG": ["ZS", "HS", "Marvel R", "MG4", "MG5"],
+  
+  "Mini": ["Cooper", "Countryman", "Clubman", "Cooper Electric"],
+  
+  "Mitsubishi": ["ASX", "Eclipse Cross", "Outlander", "Outlander PHEV", "L200"],
+  
+  "Nissan": ["Micra", "Juke", "Qashqai", "X-Trail", "Ariya", "Navara", "Leaf"],
+  
+  "Opel": [
+    "Corsa", "Corsa-e", "Astra", "Astra Electric", "Insignia",
+    "Crossland", "Grandland", "Mokka", "Mokka-e", "Combo"
+  ],
+  
+  "Ora": ["03", "07"],
+  
+  "Peugeot": [
+    "208", "e-208", "2008", "e-2008", "308", "408", "3008", "5008",
+    "Rifter", "Partner", "Traveller"
+  ],
+  
+  "Porsche": ["718", "911", "Taycan", "Macan", "Cayenne", "Panamera"],
+  
+  "Renault": [
+    "Clio", "Megane", "Megane E-Tech", "Taliant", "Austral",
+    "Captur", "Kadjar", "Koleos", "Kangoo", "Trafic",
+    "Zoe", "Twingo"
+  ],
+  
   "Seat": ["Ibiza", "Leon", "Arona", "Ateca", "Tarraco"],
-  "Skoda": ["Fabia", "Scala", "Octavia", "Superb", "Kamiq", "Karoq", "Kodiaq"],
-  "Suzuki": ["Swift", "Vitara", "S-Cross", "Ignis"],
-  "Toyota": ["Yaris", "Corolla", "Camry", "C-HR", "RAV4", "Land Cruiser", "Hilux", "Proace"],
-  "Volkswagen": ["Polo", "Golf", "Passat", "Tiguan", "T-Cross", "T-Roc", "Touareg", "Caddy", "Transporter"],
-  "Volvo": ["S60", "S90", "V60", "V90", "XC40", "XC60", "XC90"],
+  
+  "Skoda": [
+    "Fabia", "Scala", "Octavia", "Superb",
+    "Kamiq", "Karoq", "Kodiaq", "Enyaq iV"
+  ],
+  
+  "Ssangyong": ["Tivoli", "Korando", "Rexton"],
+  
+  "Subaru": ["Impreza", "XV", "Forester", "Outback", "Solterra"],
+  
+  "Suzuki": ["Swift", "Vitara", "S-Cross", "Ignis", "Jimny"],
+  
+  "Tesla": ["Model 3", "Model Y", "Model S", "Model X", "Cybertruck"],
+  
+  "Togg": ["T10X", "T10S"],
+  
+  "Toyota": [
+    "Yaris", "Yaris Cross", "Corolla", "Corolla Cross", "Camry", "GR86",
+    "C-HR", "RAV4", "Highlander", "Land Cruiser", "Land Cruiser Prado",
+    "Hilux", "Proace", "bZ4X"
+  ],
+  
+  "Volkswagen": [
+    "Polo", "Golf", "Arteon", "Passat", "Jetta",
+    "T-Cross", "T-Roc", "Tiguan", "Touareg", "Touran",
+    "Caddy", "Transporter", "ID.3", "ID.4", "ID.5", "ID.7", "ID. Buzz"
+  ],
+  
+  "Volvo": [
+    "S60", "S90", "V60", "V60 Cross Country", "V90", "V90 Cross Country",
+    "XC40", "XC40 Recharge", "XC60", "XC90", "C40 Recharge", "EX30", "EX90"
+  ],
 };
